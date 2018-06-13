d319aef7ecb580170c6f6cd4763ac4125bbbe3c6
Cody <cody@vieodesign.com>
2018-06-13 09:36:14 -0400

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
-	socket.emit('newMessage', {
-		from: 'Cody',
-		text: 'This is a message.',
-		createdAt: 123123
-	});
+	// socket.emit sends emit to single connection
+	// io.emit send emit to every connection
 
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
 	});
 
 	socket.on('disconnect', () => {
