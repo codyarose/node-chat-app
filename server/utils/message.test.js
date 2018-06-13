4a562615fb8e707d0c016a84e29c982b97ed7d40
Cody <cody@vieodesign.com>
2018-06-13 11:52:08 -0400

    Add geolocation support vie geolocation api

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-let {generateMessage} = require('./message');
+let {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate the correct message object', () => {
@@ -12,8 +12,26 @@ describe('generateMessage', () => {
 		let text = 'Message body';
 		let message = generateMessage(from, text);
 
-		expect(typeof message.createdAt).toBe('number');
 		expect(message.from).toBe(from);
 		expect(message.text).toBe(text);
+		expect(typeof message.createdAt).toBe('number');
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location object', () => {
+		// from, lat, long
+		// expect from correct
+		// createdAt is a number
+		// url prop is what you'd expect given the input
+		let from = 'Name';
+		let latitude = 123;
+		let longitude = 456;
+		let url = 'https://www.google.com/maps?q=123,456';
+		let message = generateLocationMessage(from, latitude, longitude);
+
+		expect(message.from).toBe(from);
+		expect(message.url).toBe(url);
+		expect(typeof message.createdAt).toBe('number');
 	});
 });
